f129ee21fcf184ff44ede0d48efbc21af80de40a
kamol789 <95198959+kamol789@users.noreply.github.com>
2021-11-29 12:09:55 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = '#ใส่ TOKEN HERE'; 
+ $access_token = 'IMu8sGxVqzdZryMK5cMNZ/YJJEAmVvdC573BsHQbMCkw7tt1OI8v3UNHK8kfa7dmSPzcAhj1X4SI0yHZeIjrPN78nbFfSM3c+/krBc+6kWXd1F6btTuD4wrFyPyuTAveIGAR7q5AJiW/YcOb52cnrwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => '#ใส่ ID HERE',
+        'to' => '@353xmjqe',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
